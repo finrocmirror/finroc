6425f64c6c52f417db60a59fe4c0e4b65c024407
Tobias Föhst <foehst@informatik.uni-kl.de>
2010-12-14 14:08:37 +0000

    Added preliminary svn credentials handling to finroc_get

--- scripts/perl/FINROC/rcs/subversion.pm
+++ scripts/perl/FINROC/rcs/subversion.pm
@@ -18,16 +18,20 @@ sub GetRepositoryPrefix()
     return sprintf "%s", map { chomp; s/\/[^\/]*$//; $_ } $repository{"root"};
 }
 
-sub Checkout($$)
+sub Checkout($$$$)
 {
-    my ($repository, $directory) = @_;
+    my ($repository, $directory, $username, $password) = @_;
 
     $repository = sprintf "%s/%s/trunk", GetRepositoryPrefix, $repository;
     $repository =~ s/mca2\/trunk$/mca2\/branches\/mca3_experimental/; # FIXME: ONLY FOR MCA2 LEGACY
     ERRORMSG sprintf "'%s' should be used for working copy but is a file\n", $directory if -f $directory;
     ERRORMSG sprintf "'%s' already exists\n", $directory if -e $directory;
+    
+    my $credentials = "";
+    $credentials = sprintf " --username=%s", $username if defined $username;
+    $credentials .= sprintf " --password=%s", $password if defined $password;
 
-    my $command = sprintf "svn co --ignore-externals %s %s", $repository, $directory;
+    my $command = sprintf "svn co --ignore-externals %s %s %s", $credentials, $repository, $directory;
     INFOMSG sprintf "Executing '%s'\n", $command;
     system $command;
     ERRORMSG "Command failed!\n" if $? != 0;
